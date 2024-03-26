c5ece5e4a62138ea60c0a910f91fc4ac1aea346a
Guillaume Belanger <guillaume.belanger27@gmail.com>
2024-03-26 16:57:03 -0400

    chore: ignore anchor error for matrix

--- docs/conf.py
+++ docs/conf.py
@@ -88,7 +88,8 @@ if not 'conf_py_path' in html_context and 'github_folder' in html_context:
 
 # For ignoring specific links
 linkcheck_anchors_ignore_for_url = [
-    r'https://github\.com/.*'
+    r'https://github\.com/.*',
+    "https://matrix.to/*"
 ]
 linkcheck_anchors_ignore_for_url.extend(custom_linkcheck_anchors_ignore_for_url)
 linkcheck_ignore = [r'http://.*\.mgmt/']
